a8610e60fe43e855f01da7302952b3c4302d031f
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 10:27:57 +0000

    Reset to 332b947fc25228c9b5e8f892b1e8dc7c9f6c4408 and made changes for LLM Round 1

--- gatox/enumerate/enumerate.py
+++ gatox/enumerate/enumerate.py
@@ -1,1 +1,24 @@
-Here is the revised code snippet that addresses the feedback received.
+class Enumerator:
+    def __init__(self, pat, socks_proxy=None, http_proxy=None, output_yaml=None, skip_log=False, github_url=None, output_json=None):
+        self.pat = pat
+        self.socks_proxy = socks_proxy
+        self.http_proxy = http_proxy
+        self.output_yaml = output_yaml
+        self.skip_log = skip_log
+        self.github_url = github_url
+        self.output_json = output_json
+
+    def validate_only(self):
+        pass
+
+    def self_enumeration(self):
+        pass
+
+    def enumerate_organization(self, org):
+        pass
+
+    def enumerate_repo_only(self, repo_name, large_enum=False):
+        pass
+
+    def enumerate_repos(self, repo_names):
+        pass
