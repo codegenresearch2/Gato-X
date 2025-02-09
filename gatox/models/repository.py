1b6742a2350b242603d272eb96ce2646fd58b48a
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 08:50:18 +0000

    Reset to 332b947fc25228c9b5e8f892b1e8dc7c9f6c4408 and made changes for LLM Round 1

--- gatox/models/repository.py
+++ gatox/models/repository.py
@@ -18,53 +18,64 @@ class Repository():
 
         self.name = self.repo_data['full_name']
         self.org_name = self.name.split('/')[0]
-        self.secrets = []
-        self.org_secrets = []
+        self.secrets: list[Secret] = []
+        self.org_secrets: list[Secret] = []
         self.sh_workflow_names = []
         self.enum_time = datetime.datetime.now()
 
         self.permission_data = self.repo_data['permissions']
         self.sh_runner_access = False
-        self.accessible_runners = []
-        self.runners = []
+        self.accessible_runners: list[Runner] = []
+        self.runners: list[Runner] = []
         self.pwn_req_risk = []
         self.injection_risk = []
 
     def is_admin(self):
+        """Check if the user has admin permissions."
         return self.permission_data.get('admin', False)
 
     def is_maintainer(self):
+        """Check if the user has maintain permissions."
         return self.permission_data.get('maintain', False)
 
     def can_push(self):
+        """Check if the user can push to the repository."
         return self.permission_data.get('push', False)
 
     def can_pull(self):
+        """Check if the user can pull from the repository."
         return self.permission_data.get('pull', False)
 
     def is_private(self):
+        """Check if the repository is private."
         return self.repo_data['private']
 
     def is_archived(self):
+        """Check if the repository is archived."
         return self.repo_data['archived']
 
     def is_internal(self):
+        """Check if the repository is internal."
         return self.repo_data['visibility'] == 'internal'
 
     def is_public(self):
+        """Check if the repository is public."
         return self.repo_data['visibility'] == 'public'
 
     def is_fork(self):
+        """Check if the repository is a fork."
         return self.repo_data['fork']
 
     def can_fork(self):
+        """Check if the repository allows forking."
         return self.repo_data.get('allow_forking', False)
 
     def default_path(self):
+        """Return the default path for the repository."
         return f"{self.repo_data['html_url']}/blob/{self.repo_data['default_branch']}"
 
     def update_time(self):
-        """Update timestamp."""
+        """Update the timestamp for the repository enumeration."
         self.enum_time = datetime.datetime.now()
 
     def set_accessible_org_secrets(self, secrets: list[Secret]):
@@ -75,22 +86,23 @@ class Repository():
         self.org_secrets = secrets
 
     def set_pwn_request(self, pwn_request_package: dict):
+        """Set a package for a pwn request risk."
         self.pwn_req_risk.append(pwn_request_package)
 
     def clear_pwn_request(self, workflow_name):
-        """Remove pwn request entry since it's a false positive."""
+        """Remove a pwn request entry if it's a false positive."
         self.pwn_req_risk = [element for element in self.pwn_req_risk if element['workflow_name'] != workflow_name]
 
     def has_pwn_request(self):
-        """Return True if there are any pwn request risks."""
+        """Return True if there are any pwn request risks."
         return len(self.pwn_req_risk) > 0
 
     def set_injection(self, injection_package: dict):
-        """Set injection risk package."
+        """Set a package for an injection risk."
         self.injection_risk.append(injection_package)
 
     def has_injection(self):
-        """Return True if there are any injection risks."""
+        """Return True if there are any injection risks."
         return len(self.injection_risk) > 0
 
     def set_secrets(self, secrets: list[Secret]):
@@ -119,7 +131,7 @@ class Repository():
         self.accessible_runners.append(runner)
 
     def toJSON(self):
-        """Converts the repository to a Gato JSON representation."""
+        """Converts the repository to a Gato JSON representation."
         representation = {
             "name": self.name,
             "enum_time": self.enum_time.ctime(),
