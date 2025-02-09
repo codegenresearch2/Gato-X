ce3b97eeb511ba26df04f1d30389e82a823472e0
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 08:49:51 +0000

    Reset to 332b947fc25228c9b5e8f892b1e8dc7c9f6c4408 and made changes for LLM Round 1

--- gatox/enumerate/organization.py
+++ gatox/enumerate/organization.py
@@ -48,7 +48,14 @@ class OrganizationEnum():
         organization.set_public_repos(org_public_repos)
         organization.set_private_repos(org_private_repos)
 
-        return org_public_repos + org_private_repos
+        if org_private_repos:
+            sso_enabled = self.api.validate_sso(organization.name, org_private_repos[0].name)
+            organization.sso_enabled = sso_enabled
+
+        if organization.sso_enabled:
+            return org_private_repos + org_public_repos
+        else:
+            return org_public_repos
 
     def admin_enum(self, organization: Organization):
         """Enumeration tasks to perform if the user is an org admin and the token has the necessary scopes."""
