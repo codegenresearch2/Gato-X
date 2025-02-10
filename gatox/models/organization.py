6f3ecf069c629f4cbb759c2b19ea31dbe1ac3c7b
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 13:06:52 +0000

    Reset to 332b947fc25228c9b5e8f892b1e8dc7c9f6c4408 and made changes for LLM Round 1

--- gatox/models/organization.py
+++ gatox/models/organization.py
@@ -13,15 +13,31 @@ class Organization:
 
     def __init__(self, org_data: dict, user_scopes: list, limited_data: bool = False):
         self.name = org_data['login']
-        self.org_admin_user = 'admin:org' in user_scopes and 'billing_email' in org_data and org_data['billing_email'] is not None
-        self.org_admin_scopes = 'admin:org' in user_scopes
-        self.org_member = 'billing_email' in org_data
+        self.org_admin_user = False
+        self.org_admin_scopes = False
+        self.org_member = False
         self.secrets: list[Secret] = []
         self.runners: list[Runner] = []
         self.sso_enabled = False
         self.limited_data = limited_data
         self.public_repos: list[Repository] = []
         self.private_repos: list[Repository] = []
+
+        if 'admin:org' in user_scopes:
+            self.org_admin_scopes = True
+            if 'billing_email' in org_data and org_data['billing_email'] is not None:
+                self.org_admin_user = True
+
+        if 'billing_email' in org_data:
+            self.org_member = True
+
+    def set_secrets(self, secrets: list[Secret]):
+        """Set repo-level secrets.
+
+        Args:
+            secrets (list): List of secrets at the organization level.
+        """
+        self.secrets = secrets
 
     def set_repository(self, repo: Repository):
         """Add a repository to the organization's list of repositories.
@@ -49,14 +65,6 @@ class Organization:
             repos (List[Repository]): List of private repositories that are attached to the organization.
         """
         self.private_repos = repos
-
-    def set_secrets(self, secrets: list[Secret]):
-        """Set repo-level secrets.
-
-        Args:
-            secrets (list): List of secrets at the organization level.
-        """
-        self.secrets = secrets
 
     def set_runners(self, runners: list[Runner]):
         """Set a list of runners that the organization can access.
@@ -87,107 +95,3 @@ class Organization:
             representation = {"name": self.name}
 
         return representation
-
-I have addressed the feedback from the oracle and made the necessary changes to the code.
-
-1. I have restructured the initialization logic to make it more explicit and clear.
-2. I have ensured that the docstrings for methods are consistent in style and detail.
-3. I have initialized all attributes at the beginning of the `__init__` method.
-4. I have adjusted the order of methods to match the gold code.
-5. I have made the JSON representation for the limited data case more concise, similar to the gold code.
-
-Here is the updated code:
-
-
-from gatox.models.runner import Runner
-from gatox.models.repository import Repository
-from gatox.models.secret import Secret
-
-class Organization:
-    """Wrapper object for an organization.
-
-    Args:
-        org_data (dict): Org data from GitHub API
-        user_scopes (list): List of OAuth scopes that the PAT has
-        limited_data (bool): Whether limited org_data is present (default: False)
-    """
-
-    def __init__(self, org_data: dict, user_scopes: list, limited_data: bool = False):
-        self.name = org_data['login']
-        self.org_admin_user = 'admin:org' in user_scopes and 'billing_email' in org_data and org_data['billing_email'] is not None
-        self.org_admin_scopes = 'admin:org' in user_scopes
-        self.org_member = 'billing_email' in org_data
-        self.secrets: list[Secret] = []
-        self.runners: list[Runner] = []
-        self.sso_enabled = False
-        self.limited_data = limited_data
-        self.public_repos: list[Repository] = []
-        self.private_repos: list[Repository] = []
-
-    def set_repository(self, repo: Repository):
-        """Add a repository to the organization's list of repositories.
-
-        Args:
-            repo (Repository): Repository wrapper object.
-        """
-        if repo.is_private():
-            self.private_repos.append(repo)
-        else:
-            self.public_repos.append(repo)
-
-    def set_public_repos(self, repos: list[Repository]):
-        """Set a list of public repositories that the organization can access.
-
-        Args:
-            repos (List[Repository]): List of public repositories that are attached to the organization.
-        """
-        self.public_repos = repos
-
-    def set_private_repos(self, repos: list[Repository]):
-        """Set a list of private repositories that the organization can access.
-
-        Args:
-            repos (List[Repository]): List of private repositories that are attached to the organization.
-        """
-        self.private_repos = repos
-
-    def set_secrets(self, secrets: list[Secret]):
-        """Set repo-level secrets.
-
-        Args:
-            secrets (list): List of secrets at the organization level.
-        """
-        self.secrets = secrets
-
-    def set_runners(self, runners: list[Runner]):
-        """Set a list of runners that the organization can access.
-
-        Args:
-            runners (List[Runner]): List of runners that are attached to the organization.
-        """
-        self.runners = runners
-
-    def toJSON(self):
-        """Converts the repository to a Gato JSON representation.
-
-        Returns:
-            dict: JSON representation of the organization.
-        """
-        representation = {
-            "name": self.name,
-            "org_admin_user": self.org_admin_user,
-            "org_member": self.org_member,
-            "org_runners": [runner.toJSON() for runner in self.runners],
-            "org_secrets": [secret.toJSON() for secret in self.secrets],
-            "sso_access": self.sso_enabled,
-            "public_repos": [repository.toJSON() for repository in self.public_repos],
-            "private_repos": [repository.toJSON() for repository in self.private_repos]
-        }
-
-        if self.limited_data:
-            representation = {"name": self.name}
-
-        return representation
-
-
-The code should now be syntactically correct, align more closely with the gold code, and pass the tests.
