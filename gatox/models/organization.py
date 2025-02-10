36b1aaa520a7f752941697d23ea4fc8912517e58
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 10:45:53 +0000

    Reset to 332b947fc25228c9b5e8f892b1e8dc7c9f6c4408 and made changes for LLM Round 1

--- gatox/models/organization.py
+++ gatox/models/organization.py
@@ -1,3 +1,7 @@
+from gatox.models.runner import Runner
+from gatox.models.repository import Repository
+from gatox.models.secret import Secret
+
 class Organization:
     """Wrapper object for an organization.
 
@@ -10,13 +14,14 @@ class Organization:
     def __init__(self, org_data: dict, user_scopes: list, limited_data: bool = False):
         self.name = org_data['login']
         self.org_admin_user = 'admin:org' in user_scopes and 'billing_email' in org_data and org_data['billing_email'] is not None
+        self.org_admin_scopes = 'admin:org' in user_scopes
         self.org_member = 'billing_email' in org_data
-        self.secrets: list[Secret] = []
-        self.runners: list[Runner] = []
+        self.secrets = []
+        self.runners = []
         self.sso_enabled = False
         self.limited_data = limited_data
-        self.public_repos: list[Repository] = []
-        self.private_repos: list[Repository] = []
+        self.public_repos = []
+        self.private_repos = []
 
     def set_secrets(self, secrets: list[Secret]):
         """Set repo-level secrets.
@@ -26,16 +31,21 @@ class Organization:
         """
         self.secrets = secrets
 
-    def add_repository(self, repo: Repository):
-        """Add a repository to the organization's list of repositories.
+    def set_public_repos(self, repos: list[Repository]):
+        """List of public repos for the org.
 
         Args:
-            repo (Repository): Repository wrapper object.
+            repos (List[Repository]): List of Repository wrapper objects.
         """
-        if repo.is_public():
-            self.public_repos.append(repo)
-        else:
-            self.private_repos.append(repo)
+        self.public_repos = repos
+
+    def set_private_repos(self, repos: list[Repository]):
+        """List of private repos for the org.
+
+        Args:
+            repos (List[Repository]): List of Repository wrapper objects.
+        """
+        self.private_repos = repos
 
     def set_runners(self, runners: list[Runner]):
         """Set a list of runners that the organization can access.
@@ -66,6 +76,3 @@ class Organization:
             representation = {"name": self.name}
 
         return representation
-
-
-In the updated code, I have addressed the test case feedback by removing the extraneous text from the `Organization` class implementation. I have also added docstrings to the class and methods, improved the initialization logic, added type annotations, renamed the `set_repository` method to `add_repository`, and enhanced the JSON representation method. Additionally, I have added a method to add a repository to the organization's list of repositories based on its privacy status.
